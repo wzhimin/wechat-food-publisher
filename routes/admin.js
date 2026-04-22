92ef63261bf8368829dfcad8aa756762e75c0d5c
Zhimin Wang <wangzm03@rd.netease.com>
2026-04-22 16:38:27 +0800

    fix: admin.js 添加 fs 模块引用，修复 articles/:id/recipes 接口报错

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,7 @@
 const express = require('express');
 const router = express.Router();
 const crypto = require('crypto');
+const fs = require('fs');
 const path = require('path');
 const { Op } = require('sequelize');
 const { sequelize: dbSeq } = require('../db');
